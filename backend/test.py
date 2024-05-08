ce439331a3b4ebe4e7f0201b6cb1692080532790
kanlo456 <kanson456@gmail.com>
2024-05-08 11:05:38 +0800

    updated

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO, checks, hub
-checks()
-
-hub.login('5d79aeed8058a8c6d4e201acc3a14607e6d87e5d72')
-
-if __name__ == "__main__":
-    model = YOLO('https://hub.ultralytics.com/models/mp0Diidt6AT4XEqU6FOs')
-    results = model.train()
